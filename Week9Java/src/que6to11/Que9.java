79abc648ee23b553f3539c415f9ea5223f4ed1af
doorclose <codebuster2023@gmail.com>
2024-05-01 23:47:36 +0100

    my 1st push to repo.

---
+++ Week9Java/src/que6to11/Que9.java
@@ -0,0 +1,21 @@
+package que6to11;
+
+import java.util.ArrayList;
+
+//. Write program and add all group names in to array and iterates through for  each loop.
+public class Que9 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> groupname = new ArrayList<>();
+        groupname.add("bright1");
+        groupname.add("bright2");
+        groupname.add("bright3");
+        groupname.add("bright4");
+
+        System.out.println("Groupnames are ;=>");
+        for(String groupnames : groupname);
+            System.out.println(groupname);
+        }
+    }
+
