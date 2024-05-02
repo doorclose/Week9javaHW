8435ef6ea67f9ef9980151266d1f3d6b042661d9
doorclose <codebuster2023@gmail.com>
2024-05-02 21:08:24 +0100

    my 1st push to repo.

---
+++ Week9Java/src/que6to11/Que10.java
@@ -0,0 +1,15 @@
+package que6to11;
+//Write program and add all group members names
+// in to array and iterates  through for each loop and print your name.
+public class Que10 {
+    public static void main(String[] args) {
+
+        String[] groupMembers = {"Monica", "Neha", "Chintan", "Kinjal", "Riya"};
+
+            System.out.println(groupMembers [3]);
+        }
+    }
+
+
+
+
